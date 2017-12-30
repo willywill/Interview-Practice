8b945a92b744fcf8b147f2025900c1c500016fa5
Will Germany <germany.william@gmail.com>
2017-12-30 17:46:26 -0500

    Added Question-03 Regex using Python

---
+++ Python/Question-03/README.md
@@ -0,0 +1,7 @@
+# Question 03
+
+Write a regular expression that matches the US standard MM/DD/YYYY.
+
+## Example
+
+`12/30/2017` 
---
+++ Python/Question-03/index.py
@@ -0,0 +1,14 @@
+import re
+
+def RegExResult(r):
+  return r.group(0)
+
+givenInput = r'12/30/2017'
+
+'''
+ MM/DD/YYYY - 2 digits (0-9), followed by a slash, 2 digits (0-9),
+ followed by a slash, 4 digits (0-9)
+'''
+regexExp = re.search(r'[\d{2}]+\/[\d{2}]+\/\d{4}', givenInput)
+
+print(RegExResult(regexExp))
