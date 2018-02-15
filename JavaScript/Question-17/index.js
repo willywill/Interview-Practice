10a4301eca1d3e64dde28ec8749c99e8d069c62f
Will Germany <germany.william@gmail.com>
2018-02-14 22:41:28 -0500

    Shortend code, fixed line endings

--- JavaScript/Question-17/index.js
+++ JavaScript/Question-17/index.js
@@ -1,5 +1,6 @@
-const customMultiply = (x, y) => [...Array(y).keys()].map(z => z + 3).reduce((a, b, c) => a + x , 0)
+const customMultiply = (x, y) => [...Array(y).keys()].map(z => z + 3).reduce(a => a + x , 0)
 
-const n = customMultiply(3, 3)
+const n = customMultiply(5, 5)
 
 console.log(n)
+
