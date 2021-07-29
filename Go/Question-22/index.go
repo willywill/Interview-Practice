00e71c5696bf4dcfb674cfa9443662778a02b440
Will Germany <germany.william@gmail.com>
2021-07-28 23:31:56 -0400

    Memoization for Question 22

--- Go/Question-22/index.go
+++ Go/Question-22/index.go
@@ -4,7 +4,6 @@ import (
 	"strings"
 )
 
-// TODO: This is a naive solution, we can add memoization to optimize it
 func WordBreak(targetWord string, words []string) bool {
 	if targetWord == "" {
 		return true
@@ -26,6 +25,41 @@ func WordBreak(targetWord string, words []string) bool {
 	return false
 }
 
+func WordBreakCached(targetWord string, words []string, cache map[string]bool) bool {
+	if targetWord == "" {
+		return true
+	}
+
+	// If we have a value in the cache, return it
+	val, ok := cache[targetWord]
+	if ok {
+		return val
+	}
+
+	for _, word := range words {
+		if strings.HasPrefix(targetWord, word) {
+			// Get the suffix of targetWord starting from the length of word to the end of targetWord
+			suffix := targetWord[len(word):]
+
+			// Recursively call WordBreak with the suffix
+			if WordBreakCached(suffix, words, cache) {
+				// Cache the result before returning to the caller
+				cache[targetWord] = true
+				return true
+			}
+		}
+	}
+
+	// Tried everything, and no match was found
+	cache[targetWord] = false
+	return false
+}
+
+func WordBreakWithMemoization(targetWord string, words []string) bool {
+	cache := make(map[string]bool)
+	return WordBreakCached(targetWord, words, cache)
+}
+
 func main() {
 	words1 := []string{"ab", "abc", "cd", "def", "abcd"}
 	targetWord1 := "abcdef"
@@ -42,9 +76,9 @@ func main() {
 	words5 := []string{"leet", "code"}
 	targetWord5 := "leetcode"
 
-	println("Example 1: ", WordBreak(targetWord1, words1))
-	println("Example 2: ", WordBreak(targetWord2, words2))
-	println("Example 3: ", WordBreak(targetWord3, words3))
-	println("Example 4: ", WordBreak(targetWord4, words4))
-	println("Example 5: ", WordBreak(targetWord5, words5))
+	println("Example 1: ", WordBreakWithMemoization(targetWord1, words1))
+	println("Example 2: ", WordBreakWithMemoization(targetWord2, words2))
+	println("Example 3: ", WordBreakWithMemoization(targetWord3, words3))
+	println("Example 4: ", WordBreakWithMemoization(targetWord4, words4))
+	println("Example 5: ", WordBreakWithMemoization(targetWord5, words5))
 }
